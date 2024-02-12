55f34f8302f8a890de3a3254c530d113556b0968
Rameshacharya22 <acharya232ramesh@gmail.com>
2024-02-12 17:25:03 +0545

    Add fake data through seeder for testing purposes.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,16 +12,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+//         \App\Models\User::factory(10)->create();
 
 
         \App\Models\User::factory()->create(
             [
                 'name' => 'Admin',
-            'email' => 'admin@test.test',
-            'password' => bcrypt('12345678'),
-            'role'=>'Admin'
+                'email' => 'admin@erp.test',
+                'password' => bcrypt('12345678'),
+                'role' => 'Admin'
             ]);
+
+        \App\Models\User::factory()->create(
+            [
+                'name' => 'User',
+                'email' => 'user@erp.test',
+                'password' => bcrypt('user'),
+                'role' => 'User'
+            ]
+        );
+
+        \App\Models\Employee::create(
+            [
+                'first_name' => 'User',
+                'email' => 'user@erp.test',
+            ]
+        );
+
+        \App\Models\User::factory()->create(
+            [
+                'name' => 'Aashish',
+                'email' => 'aashish@erp.test',
+                'password' => bcrypt('user'),
+                'role' => 'User'
+            ]
+        );
+
+        \App\Models\Employee::create(
+            [
+                'first_name' => 'Aashish',
+                'email' => 'aashish@erp.test',
+            ]
+        );
+
+
         $this->call(DepartmentSeeder::class);
     }
 }
