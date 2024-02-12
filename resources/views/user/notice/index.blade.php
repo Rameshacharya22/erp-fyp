ef4933a7471e37b662d7abb7c65c4a748962058b
Rameshacharya22 <acharya232ramesh@gmail.com>
2024-02-12 11:48:35 +0545

    Add CRUD operations for Notice

---
+++ resources/views/user/notice/index.blade.php
@@ -0,0 +1,112 @@
+@extends('adminlte::page')
+
+@section('title', 'Notice')
+
+
+@section('content_header')
+
+
+    {{-- <div class="notice-box">
+            <div class="notice-header">
+                <div class="title">
+                    adipisicing elit. Fuga magnam natus eveniet </div>
+                <div class="date">
+                    <i class="fas fa-calendar-alt"></i> 09-09-2024
+                </div>
+            </div>
+            <div class="notice-info">
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga dolor, sit amet consectetur
+                    adipisicingonsectetur adipisicing
+                    elit.
+                    Fuga magnam natus evenietonsectetur adipisicing
+                    elit.
+                    Fuga magnam natus evenietonsectetur adipisicing
+                    elit.
+                    Fuga magnam natus eveniet
+                    elit.
+                    Fuga magnam natus eveniet
+                </p>
+
+                <a href="" class="btn"> View Detail</a>
+            </div>
+
+        </div> --}}
+
+    {{-- <div class="notice-box">
+            <div class="notice-header">
+                <div class="title">
+                    adipisicing elit. Fuga magnam natus eveniet </div>
+                <div class="date">
+                    <i class="fas fa-calendar-alt"></i> 09-09-2024
+                </div>
+            </div>
+            <div class="notice-info">
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga dolor, sit amet consectetur adipisicing
+                    elit.
+                    Fuga magnam natus eveniet
+                </p>
+
+                <a href="" class="btn"> View Detail</a>
+            </div>
+
+        </div> --}}
+
+    {{-- <div class="notice-box">
+            <div class="notice-header">
+                <div class="title">
+                    adipisicing elit. Fuga magnam natus eveniet </div>
+                <div class="date">
+                    <i class="fas fa-calendar-alt"></i> 09-09-2024
+                </div>
+            </div>
+            <div class="notice-info">
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga dolor, sit amet consectetur adipisicing
+                    elit.
+                    Fuga magnam natus eveniet
+                </p>
+
+                <a href="" class="btn"> View Detail</a>
+            </div>
+
+        </div> --}}
+
+    {{-- <div class="notice-box">
+            <div class="notice-header">
+                <div class="title">
+                    adipisicing elit. Fuga magnam natus eveniet </div>
+                <div class="date">
+                    <i class="fas fa-calendar-alt"></i> 09-09-2024
+                </div>
+            </div>
+            <div class="notice-info">
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga dolor, sit amet consectetur adipisicing
+                    elit.
+                    Fuga magnam natus eveniet
+                </p>
+
+                <a href="" class="btn"> View Detail</a>
+            </div>
+
+        </div> --}}
+    </div>
+
+
+@stop
+
+@section('content')
+
+
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+
+@stop
