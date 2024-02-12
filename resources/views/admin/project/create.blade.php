f38019366114beec44ec2edbee7bb8b6c2fac20a
Rameshacharya22 <acharya232ramesh@gmail.com>
2024-02-12 17:26:53 +0545

    Update form textarea and date input handling.

--- resources/views/admin/project/create.blade.php
+++ resources/views/admin/project/create.blade.php
@@ -24,46 +24,50 @@
                 </div>
 
                 <div class="form-group col-md-6 mt-3">
+                    <label for="title">Project Member</label>
+                    <select  class="form-control" id="employee_id" name="employee_ids[]" required multiple>
+                        @foreach ($employees as $employee) employees
+                        <option value="{{ $employee->id }}">{{ $employee->first_name }}
+                        </option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="form-row ">
+                <div class="form-group col-md">
                     <label for="description">Project Description</label>
-                    <input type="text" value="{{old('description')}}" class="form-control" id="description" name="description" required>
+{{--                    <textarea class="form-control" value="{{old('description')}}" name="description" id="description" cols="30" rows="4"></textarea>--}}
+                    <textarea class="form-control" id="description" name="description" cols="30" rows="4" required>{{ old('description') }}</textarea>
+
                 </div>
-
-
             </div>
 
             <div class="form-row">
 
-                <div class="form-group col-md-6 mt-3">
+                <div class="form-group col-md-6 ">
                     <label for="started_at">Started Date</label>
-                    <input type="date" value="{{old('started_at')}}" class="form-control" id="started_at" name="started_at" required>
+                    <input type="date" value="{{old('started_at')}}" class="form-control" id="started_at" name="started_at" required min="{{\Carbon\Carbon::now()->format('Y-m-d')}}">
                 </div>
 
                 <div class="form-group col-md-6">
-                    <label for="deadline_at">Deadline Date</label>
-                    <input type="date" value="{{old('deadline_at')}}" class="form-control" id="deadline_at" name="deadline_at" required>
+                    <label for="deadline_at"> Deadline Date</label>
+                    <input type="date" value="{{old('deadline_at')}}" class="form-control" id="deadline_at" name="deadline_at" min="{{\Carbon\Carbon::now()->format('Y-m-d')}}" required>
                 </div>
             </div>
 
             <div class="form-row">
 
                 <div class="form-group col-md-6">
-                    <label for="completion_time">Completion Time</label>
-                    <input type="time" value="{{old('completion_time')}}" class="form-control" id="completion_time" name="completion_time" required>
+                    <label for="completion_time"> Completion Time </label>
+                    <input type="time" value="{{old('completion_time')}}" class="form-control" id="completion_time" name="completion_time" >
                 </div>
                 <div class="form-group col-md-6">
                     <label for="completion_at">Completed Date</label>
-                    <input type="date" class="form-control" value="{{old('completion_at')}}" id="completion_at" name="completion_at" required>
+                    <input type="date" class="form-control" value="{{old('completion_at')}}" id="completion_at" name="completion_at" min="{{\Carbon\Carbon::now()->format('Y-m-d')}}" required>
                 </div>
             </div>
 
-            {{--
-            title
-        description
-        started_at
-        deadline_at
-        completion_time
-        completed_at
-        --}}
+
 
 
 
@@ -82,6 +86,7 @@
 @section('css')
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <!-- <link rel="stylesheet" href="/css/admin_custom.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@2.0.1/dist/css/multi-select-tag.css">
 @stop
 
 @section('js')
@@ -89,7 +94,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-    <!-- <script>
-        console.log('Hi!');
-    </script> -->
+    <script src="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@2.0.1/dist/js/multi-select-tag.js"></script>
+    <script>
+        new MultiSelectTag('employee_id')  // id
+    </script>
 @stop
--- resources/views/admin/project/edit.blade.php
+++ resources/views/admin/project/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'Add Project')
+@section('title', 'Edit Project')
 
 <!-- @section('content_header')
 
@@ -24,25 +24,34 @@
                     <label for="title">Project Title</label>
                     <input type="text" class="form-control" value="{{$project->title}}" id="title" name="title" required>
                 </div>
-
                 <div class="form-group col-md-6 mt-3">
-                    <label for="description">Project Description</label>
-                    <input type="text" class="form-control" id="description" value="{{$project->description}}" name="description" required>
+                    <label for="title">Project Member</label>
+                    <select  class="form-control" id="employee_id" name="employee_ids[]" required multiple>
+                        @foreach ($employees as $employee) employees
+                        <option value="{{ $employee->id }}">{{ $employee->first_name }}
+                        </option>
+                        @endforeach
+                    </select>
                 </div>
+            </div>
 
-
+            <div class="form-row ">
+                <div class="form-group col-md">
+                    <label for="description">Project Description</label>
+                    <textarea class="form-control"  name="description" id="description" cols="30" rows="4">{{$project->description}}</textarea>
+                </div>
             </div>
 
             <div class="form-row">
 
-                <div class="form-group col-md-6 mt-3">
+                <div class="form-group col-md-6 ">
                     <label for="started_at">Started Date</label>
-                    <input type="date" class="form-control" id="started_at" value="{{$project->started_at}}" name="started_at" required>
+                    <input type="date" class="form-control" id="started_at" value="{{$project->started_at}}" min="{{\Carbon\Carbon::now()->format('Y-m-d')}}" name="started_at" required>
                 </div>
 
                 <div class="form-group col-md-6">
                     <label for="deadline_at">Deadline Date</label>
-                    <input type="date" class="form-control" id="deadline_at" value="{{$project->deadline_at}}" name="deadline_at" required>
+                    <input type="date" class="form-control" id="deadline_at" value="{{$project->deadline_at}}" name="deadline_at" min="{{\Carbon\Carbon::now()->format('Y-m-d')}}" required>
                 </div>
             </div>
 
@@ -54,24 +63,24 @@
                 </div>
                 <div class="form-group col-md-6">
                     <label for="completion_at">Completed Date</label>
-                    <input type="date" class="form-control" id="completion_at" value="{{$project->completion_at}}" name="completion_at" required>
+                    <input type="date" class="form-control" id="completion_at" value="{{$project->completion_at}}"  name="completion_at" min="{{\Carbon\Carbon::now()->format('Y-m-d')}}" required>
                 </div>
             </div>
 
-            {{-- 
+            {{--
             title
-        description 
+        description
         started_at
         deadline_at
         completion_time
-        completed_at 
+        completed_at
         --}}
 
 
-
+            <button type="submit" class="btn btn-primary">Update</button>
+            <a href="{{ route('employee.index') }}"><button type="submit" class="btn btn-secondary">Back</button></a>
     </div>
-    <button type="submit" class="btn btn-primary">Update</button>
-    <a href="{{ route('employee.index') }}"><button type="submit" class="btn btn-secondary">Back</button></a>
+
     </form>
     </div>
 
@@ -85,6 +94,8 @@
 @section('css')
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <!-- <link rel="stylesheet" href="/css/admin_custom.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@2.0.1/dist/css/multi-select-tag.css">
+
 @stop
 
 @section('js')
@@ -92,7 +103,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-    <!-- <script>
-        console.log('Hi!');
-    </script> -->
+    <script src="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@2.0.1/dist/js/multi-select-tag.js"></script>
+    <script>
+        new MultiSelectTag('employee_id')  // id
+    </script>
 @stop
